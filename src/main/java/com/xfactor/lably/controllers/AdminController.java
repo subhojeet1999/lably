a1cac0c2be3f39695416e511f7a98a5ddd11de28
Subhojeet Roy <subhojeet1999@gmail.com>
2021-04-29 23:20:31 +0530

    Update AdminController.java

--- src/main/java/com/xfactor/lably/controllers/AdminController.java
+++ src/main/java/com/xfactor/lably/controllers/AdminController.java
@@ -22,38 +22,7 @@ public class AdminController {
 
     ArrayList<Admin> admins = new ArrayList<>();
 
-    // @RequestMapping(method = RequestMethod.GET)
- /*   @GetMapping
-    public String hello() {
-        return "Greetings from XFACTOR!!!";
-    }
-
-    @GetMapping("/hello/{name}")
-    public String helloName(@PathVariable String name) {
-        return "Greetings from " + name + "!!!";
-    }
-
-    @GetMapping("/hello2")
-    public String helloName2(@RequestParam String name, @RequestParam String age) {
-        return "Greetings from hello2 " + name + "!!!" + age;
-    }
-
-    @GetMapping("/hello3")
-    public Map<String, String> helloName3(@RequestParam String name, @RequestParam String age) {
-        Map<String, String> respoMap = new HashMap<>();
-        respoMap.put("name", name);
-        respoMap.put("age", age);
-        return respoMap;
-    }
-
-    @GetMapping("/hello4")
-    public ArrayList helloName4(@RequestParam String name, @RequestParam String age) {
-        ArrayList arrayList = new ArrayList<>();
-        arrayList.add(name);
-        arrayList.add(age);
-        return arrayList;
-    }*/
-
+   
     @GetMapping("/getAdmin")
     public ArrayList<Admin> getAdmin() {
         return admins;
@@ -88,51 +57,5 @@ public class AdminController {
         if(f==true) return ad;
         else return null;
     }
-
-   /* @PostMapping("/addTests")
-    public Tests addLab(@RequestBody Tests tests) {
-        String name = lab.getName();
-        name = "NAME - " + name + "\n";
-        lab.setName(name);
-        //labs.add(lab);
-        return lab;
-    }
-
-    @PostMapping("/addAdmin")
-    public Lab addLab(@RequestBody Lab lab) {
-        String name = lab.getName();
-        name = "Hello " + name + "\n";
-        address = "";
-       // lab.setName(name);
-       // labs.add(lab);
-       // return lab;
-    }
-    @PostMapping("/addCustomer")
-    public Lab addLab(@RequestBody Lab lab) {
-        String name = lab.getName();
-        name = "Hello " + name;
-        lab.setName(name);
-        labs.add(lab);
-        return lab;
-    }
-
-    // // http://localhost:8080/test/hello/xfactor
-    // @GetMapping("/hello/{name}")
-    // @ResponseBody
-    // public String index_greetings(@PathVariable String name) {
-    // return "Greetings :" + name;
-    // }
-
-    // // http://localhost:8080/test/hello2?id=16
-    // @GetMapping("/hello2")
-    // @ResponseBody
-    // public String getFoos(@RequestParam String id) {
-    // return "ID: " + id;
-    // }
-
-    // @PostMapping("/employees")
-    // Employee newEmployee(@RequestBody Employee newEmployee) {
-    // return repository.save(newEmployee);
-    // }*/
 
 }
